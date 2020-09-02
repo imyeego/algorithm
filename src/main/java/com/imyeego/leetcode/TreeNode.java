50834b0133018e4da549c81a3559e5ba18212492
imyeego <imyeego@gmail.com>
2020-09-02 14:38:41 +0800

    add leetcode item

---
+++ src/main/java/com/imyeego/leetcode/TreeNode.java
@@ -0,0 +1,17 @@
+package com.imyeego.leetcode;
+
+/**
+ * @authur : liuzhao
+ * @date : 2020/9/2
+ * @time : 下午 2:33
+ * @Des :
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
