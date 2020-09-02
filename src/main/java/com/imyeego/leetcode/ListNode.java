0883e81b5566710b36e698e1bc075bd802259e31
imyeego <imyeego@gmail.com>
2020-09-02 14:32:40 +0800

    add leetcode item

---
+++ src/main/java/com/imyeego/leetcode/ListNode.java
@@ -0,0 +1,16 @@
+package com.imyeego.leetcode;
+
+/**
+ * @authur : liuzhao
+ * @date : 2020/9/2
+ * @time : 下午 2:25
+ * @Des :
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
